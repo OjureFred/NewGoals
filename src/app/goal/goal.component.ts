4b63a553127060f9e6952386fcfae9624ebf6077
Fredrick Ojure <fredojure@hotmail.com>
2020-10-23 16:34:13 +0300

    Added inbuilt pipes Date and uppercase

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -9,12 +9,12 @@ import { Goal } from '../goal';
 export class GoalComponent implements OnInit {
 
   goals: Goal[] = [
-    new Goal(1, 'Watch Finding Nimo',  'Find an online version and stream Merlin Finds His son'),
-    new Goal(2, 'Buy cookies', 'I have to buy cookis for my sons friends coming to play tomorrow'),
-    new Goal(3, 'Get new phone case', 'This is for Davids birthday coming up soon'),
-    new Goal(4, 'Get Dog Food', 'Puppy food is finished'),
-    new Goal(5, 'Solve math homework', 'I need to practice before exams'),
-    new Goal(6, 'Plan for Angular Tutorial over the weekend', 'I need to master Angular and improve skills'),
+    new Goal(1, 'Watch Finding Nimo',  'Find an online version and stream Merlin Finds His son', new Date(2020, 3, 14)),
+    new Goal(2, 'Buy cookies', 'I have to buy cookis for my sons friends coming to play tomorrow', new Date(2019, 6, 9)),
+    new Goal(3, 'Get new phone case', 'This is for Davids birthday coming up soon', new Date(2022, 1, 12)),
+    new Goal(4, 'Get Dog Food', 'Puppy food is finished', new Date(2019, 0, 18)),
+    new Goal(5, 'Solve math homework', 'I need to practice before exams', new Date(2019, 2, 14)),
+    new Goal(6, 'Plan for Angular Tutorial over the weekend', 'I need to master Angular and improve skills', new Date(2030, 3, 21)),
   ];
 
   toggleDetails(index) {
