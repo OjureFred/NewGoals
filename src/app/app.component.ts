33765b68a18e249bb15e26bf626df4e0f474ab7c
Fredrick Ojure <fredojure@hotmail.com>
2020-10-23 11:23:35 +0300

    Created Goal object and added a Goal[] array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Goal } from './goal';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goal = 'Watch Finding Nemo';
+  goals: Goal[] = [
+    { id: 1, name: 'Watch Finding Nimo' },
+    { id: 2, name: 'Buy cookies' },
+    { id: 3, name: 'Get new phone case' },
+    { id: 4, name: 'Get Dog Food' },
+    { id: 5, name: 'Solve math homework' },
+    { id: 6, name: 'Plan for Angular Tutorial over the weekend' },
+  ];
+
 }
